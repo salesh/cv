61d1cbaa651f9e3430cbf7fa3084d0501c748dde
Sasa <sasa.cvetkovic@yahoo.com>
2020-02-07 23:34:27 +0100

    Updates

---
+++ precache-manifest.5f3063b78bb7fac6687f7b598d7184c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073b64acde643a2e208e086d4eadf7d3",
+    "url": "/salesh.github.io/index.html"
+  },
+  {
+    "revision": "b684058e7f1aa0c102cc",
+    "url": "/salesh.github.io/static/css/main.a4a3371e.chunk.css"
+  },
+  {
+    "revision": "e0664b0c55409369a68a",
+    "url": "/salesh.github.io/static/js/2.5882fb55.chunk.js"
+  },
+  {
+    "revision": "2bcf73e3839e96bb488d3fc4da7ab52a",
+    "url": "/salesh.github.io/static/js/2.5882fb55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b684058e7f1aa0c102cc",
+    "url": "/salesh.github.io/static/js/main.6f938da8.chunk.js"
+  },
+  {
+    "revision": "a1bdbaededb3ed7ddf15",
+    "url": "/salesh.github.io/static/js/runtime-main.582d79c7.js"
+  },
+  {
+    "revision": "725044255354bddf646680147775047d",
+    "url": "/salesh.github.io/static/media/profile.72504425.jpeg"
+  }
+]);
